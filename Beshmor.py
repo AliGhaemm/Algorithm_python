222208828b50c7a27f7fb6d319a502c974119470
Ali <115380959+AliGhaemm@users.noreply.github.com>
2024-03-12 12:21:41 +0330

    Delete Beshmor.py

--- Beshmor.py
+++
@@ -1,8 +0,0 @@
-T = int(input())
-for i in range(T):
-    n, K = map(int, input().split())
-    arr = list(map(int, input().split()))
-    ps = [0 for _ in range(n)]
-    ps[0] = arr[0]
-    for j in range(1, n):
-        ps[j] = arr[j] + ps[j-1]
