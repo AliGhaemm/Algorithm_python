53d922c5a19506edfe95e79d7f4cd2f674b0a461
Ali <115380959+AliGhaemm@users.noreply.github.com>
2024-03-12 12:24:14 +0330

    Delete Edghamat.py

--- Edghamat.py
+++
@@ -1,8 +0,0 @@
-import MergeSort
-
-n, k = list(map(int, input().split()))
-arr = [[None for _ in range(n)] for _ in range(k)]
-for i in range(k):
-    arr[i][:] = list(map(int, input().split()))
-
-
